10788aaf09646d4b0dabc70f7ada7b038751a4e6
daniel anggrianto <daniela@aweber.com>
2014-02-19 15:11:40 -0500

    installing bats hadler

--- site-cookbooks/selenium-grid/recipes/bats-handler.rb
+++ site-cookbooks/selenium-grid/recipes/bats-handler.rb
@@ -1,5 +1,22 @@
 include_recipe "chef_handler"
 
+package 'git-core'
+
+# Accessing an external source is far from ideal but this gives us something to
+# iterate on.
+git '/root/bats' do
+  repository 'https://github.com/sstephenson/bats'
+  action :checkout
+  notifies :run, 'execute[install BATS]'
+end
+
+# Execute only when bats is checked out.
+execute 'install BATS' do
+  command '/root/bats/install.sh /usr/local'
+  action :nothing
+end
+
+
 # Install `bats-chef-handler` gem during the compile phase
 chef_gem "bats-chef-handler"
 
